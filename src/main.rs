e8aea2a1f3449322cc65052bfb15d6a72ce21133
yaowenqiang <yaowenqiang112@163.com>
2019-06-17 23:19:08 +0800

    add variable demo

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,9 @@ fn main() {
     let x = 100;
     println!("Hello, world!");
     println!("x = {}", x);
+    let a_float = 64.1;
+    let b: f32 = 64.1;
+    println!("a_float = {}", a_float);
+    println!("b = {}", b);
+    
 }
