47fbabb34f47e0bbf25c074558de11abe3fca1e3
yaowenqiang <yaowenqiang111@163.com>
2019-06-20 02:30:00 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    //let x = 5;
+    let mut x = 5;
+    let y = 7;
+    x = 4;
+    println!("{}", x);
+
+    let tup: (u8, i8, f32) = (1, -1, 3.5);
+
+    if x < y {
+        println!("{} is smaller than {}",x, y )
+    } else {
+        println!("{} is bigger than  than {}",x, y )
+    }
+
+}
