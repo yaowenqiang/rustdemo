f770150304dde688dede735e65f8258b700c7a87
yaowenqiang <yaowenqiang111@163.com>
2019-06-20 02:41:23 +0800

    add for, while loop demo

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,23 @@ fn main() {
         println!("{} is bigger than  than {}",x, y );
     }
 
-    loop {
-        println!("Rust");
+//    loop {
+//        println!("Rust");
+//    }
+
+    for x in 0..10 {
+        println!("{}", x);
+    }
+
+    let mut end = false;
+    let mut counter = 1;
+    while end == false {
+        println!("Counter is: {}", counter);
+
+        if counter == 10 {
+            end = true;
+        }
+        counter = counter + 1;
     }
 
 }
