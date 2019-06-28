d570d888f7f7391fdec911f0c80ded2dea2c4526
yaowenqiang <yaowenqiang111@163.com>
2019-06-29 04:10:33 +0800

    add cargo test demo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 mod my;
 extern crate some_crate;
+
+#[should_panic]
+#[test]
+
+fn test_user_structure(){
+    let new_user = some_crate::User{
+        name:"Dave".to_string(),
+        email: "dave@email.com".to_string(),
+        age: 32,
+        user_type: some_crate::UserType::Guest
+    };
+    assert_eq!("Dave".to_string(), new_user.name );
+
+}
 //mod my;
 /*
 #[derive(Debug)]
