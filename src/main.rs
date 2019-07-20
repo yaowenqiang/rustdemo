90c27c47b507abc5aa4329e792076024325db3cb
yaowenqiang <yaowenqiang111@163.com>
2019-07-21 00:39:07 +0800

    add traits demo

--- src/main.rs
+++ src/main.rs
@@ -360,6 +360,7 @@ fn main() {
     methods();
     closures();
     hof();
+    traits();
 }
 
 fn operators() {
@@ -962,4 +963,64 @@ fn hof()
              .fold(0, |sum, x | sum + x);
 
     println!("the sum is {}", sum2);
+}
+
+
+trait Animal {
+    fn create(name: &'static str) -> Self;
+    fn name(&self) -> &'static str;
+    fn talk(&self)
+    {
+        println!("{} cannot talk", self.name());
+    }
+}
+
+struct Human {
+    name: &'static str
+}
+
+impl Animal for Human
+{
+    fn create (name: &'static str) -> Human
+    {
+        Human{name: name}
+    }
+
+    fn name(&self) -> &'static str
+    {
+        self.name
+    }
+    fn talk(&self) 
+    {
+        println!("{} says hello",self.name());
+    }
+}
+trait Summable <T>{
+    fn sum(&self)  -> T;
+}
+impl Summable<i32> for Vec<i32>
+{
+    fn sum(&self) -> i32
+    {
+        let mut result:i32  = 0;
+        for x in self
+        {
+            result += *x;
+        }
+        return  result;
+    }
+
+}
+fn traits()
+{
+   let h = Human::create("john");
+   let h2 = Human{name:"jack"};
+   let h3:Human = Animal::create("haha");
+   h.talk();
+   h2.talk();
+   h3.talk();
+
+   let a = vec![1,2,3];
+   println!("sum = {}", a.sum());
+
 }
