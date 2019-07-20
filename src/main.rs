6ea52e869d4b419b8a3ba8babde1a99df4c59727
yaowenqiang <yaowenqiang111@163.com>
2019-07-20 17:16:52 +0800

    ad array operation demo

--- src/main.rs
+++ src/main.rs
@@ -348,6 +348,7 @@ fn main() {
     enums();
     unions();
     option();
+    arrays_fn();
 }
 
 fn operators() {
@@ -591,6 +592,48 @@ fn option ()
     }
     // if let / while let
     if let Some(z) = result{println!("z = {}", z)};
+}
+fn arrays_fn()
+{
+    let mut a:[i32;5] = [1,2,3,4,5];
+    let mut b         = [1,2,3,4,5];
+    println!("a has {} elements, first is {}", a.len(), a[0]);
+    a[0] = 123;
+    println!("a has {} elements, first is {}", a.len(), a[0]);
+    println!("{:?}",a);
+    if a != [1,2,3,4,5] {
+        println!("does not match");
+    }
+
+    let b = [1; 10];
+    let c = [1u16; 10];
+    println!("{:?}",b);
+    for i in 0..b.len()
+    {
+        println!("b[{}] = {}", i, b[i]);
+    }
+
+    println!("b took up {} bytes ", mem::size_of_val(&b) );
+    println!("c took up {} bytes ", mem::size_of_val(&c) );
+
+    let mtx:[[f32;3];2] = [
+        [1.0,0.0,0.0],
+        [0.0,2.0,3.0]
+    ];
+    println!("{:?}", mtx);
+
+    for i in 0..mtx.len()
+    {
+        for j in 0..mtx[i].len() {
+            if i == j {
+                println!("mtx[{}][{}] = {}",i ,j , mtx[i][j]);
+            }
+        }
+    }
+
+
+
+
 }
 
 
