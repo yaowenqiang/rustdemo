11484f424ec053fc2c2daa7d855a3356af04029d
yaowenqiang <yaowenqiang111@163.com>
2019-07-28 00:09:54 +0800

    add more dynamic dispatch demo

--- src/main.rs
+++ src/main.rs
@@ -383,6 +383,17 @@ fn main() {
 
     print_it_too(&cc);
     print_it_too(&dd);
+    let shapes:[&Shape; 4] = [
+        &Circle {radius: 1.0},
+        &Circle {radius: 2.0},
+        &Square {side: 3.0},
+        &Square {side: 4.0},
+    ];
+
+    for (i, shape) in shapes.iter().enumerate() {
+        println!("Shape #{} has area {}", i, shape.area());
+    }
+
 }
 
 fn operators() {
@@ -1097,3 +1108,28 @@ fn print_it_too(z: &Printable)
 {
     println!("{}", z.format() );
 }
+
+struct Circle {
+    radius: f64
+}
+struct Square {
+    side: f64
+}
+
+trait Shape {
+    fn area(&self) -> f64;
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 
+    {
+        self.radius * self.radius * std::f64::consts::PI
+    }
+}
+impl Shape for Square {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+
