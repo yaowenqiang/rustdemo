0e9a525f2624c551737bf56905eb4417a20e6be7
yaowenqiang <yaowenqiang111@163.com>
2019-08-24 11:17:46 +0800

    add more vector operation demo, include iteration ,modifing and so on

--- src/collections.rs
+++ src/collections.rs
@@ -18,9 +18,29 @@ pub fn show_vctor () {
     }
 
     let v3 = vec![1,2,3,4,5];
-    let does_not_exist = v3[200];
-    println!("{}", does_not_exist);
+    //let does_not_exist = v3[200];
+    //println!("{}", does_not_exist);
+    let mut first =  v3[0];
+    //v3.push(100);
+    println!("{}", first);
+    first = 4;
+    println!("{}", first);
     //let does_not_exist = v3.get(100);
     //println!("{}", does_not_exist);
+    //
+
+    let v = vec![100,25,57];
+    //for i in v {
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v2 = vec![100, 32, 57];
+
+    for i in &mut v2 {
+        *i += 100;
+        println!("{}", i);
+    }
+
 
 }
