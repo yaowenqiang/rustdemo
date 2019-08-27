23b868505f5e34c1a9e67ea4280dfe6da71756ce
yaowenqiang <yaowenqiang111@163.com>
2019-08-28 02:22:07 +0800

    add HashMap insert, update access demo

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -23,4 +23,46 @@ pub fn show_hash_map()
     println!("{:#?}", map);
     //println!("{}", field_name);
     //println!("{}", field_value);
+
+    let mut scores3 = HashMap::new();
+    scores3.insert(String::from("Blue"), 10);
+    scores3.insert(String::from("Yellow"), 50);
+    scores3.insert(String::from("Yellow"), 70);
+
+    let team_name = String::from("Blue");
+
+    let score = scores.get(&team_name);
+
+    println!("{:?}", score);
+
+    for (key, value)  in  &scores3 {
+        println!("{} -> {}", key, value);
+    }
+
+
+    //insert if key does not exist
+    scores3.entry(String::from("red")).or_insert(100);
+    scores3.entry(String::from("Black")).or_insert(100);
+    println!("-------------------------");
+    for (key, value)  in  &scores3 {
+        println!("{} -> {}", key, value);
+    }
+    println!("-------------------------");
+
+
+    println!("{:#?}", &scores3);
+
+
+    let text = "Hello world wonderful world";
+
+    let mut map = HashMap::new();
+    for word in text.split_whitespace()  {
+        let count = map.entry(word).or_insert(0); 
+        *count += 1;
+    }
+
+
+    println!("{:#?}", &map);
+
 }
+
