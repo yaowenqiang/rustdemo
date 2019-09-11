f15a822d12f17611cb3698d37970e4dabdb1a6e4
yaowenqiang <yaowenqiang111@163.com>
2019-09-12 03:24:32 +0800

    add more lifecycle demos

--- src/my/mod.rs
+++ src/my/mod.rs
@@ -9,4 +9,40 @@ pub fn life_cycle_demo() {
         r = &x;
     }
     println!("r : {}", r);
+
+    {
+        let x = 5;
+        let r = &x;
+        println!("r:{}", r)
+    }
+}
+pub fn function_liftcycle () {
+    let string1 = String::from("abcd");
+    let string2 = String::from("xyz");
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest2(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result );
+    }
+}
+
+pub fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x   
+    } else {
+        y
+    }
+}
+
+fn longest2<`a>(x: &`a str, y: &`a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
